7f772a342a7343bafa3435896322072fd5c4c50e
nginx <3rdoperson@gmail.com>
2022-09-20 11:06:28 +0430

    session 11

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -30,21 +30,29 @@ const url = require('url')
 // console.log('Will read file')
 
 ///////////////////////////////
-// SERVER
 
+
+// SERVER
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
     const pathName = req.url
 
+    // Overview  page
     if(pathName === '/' || pathName === '/overview') {
         res.end('This is the OVERVIEW')
+
+    // Product page
     } else if(pathName === '/product') {
         res.end('This is the PRODUCT')
+
+    // API
     } else if (pathName === '/api') {
         res.writeHead(200, {'Content-type':'application/json'})
         res.end(data)
+
+    // NOT found
     }else {
         res.writeHead(404, {
             'Content-type':'text/html',
