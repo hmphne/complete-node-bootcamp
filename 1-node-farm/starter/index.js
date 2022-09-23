f0c4dc3297c4021b463dfdaf447049581499e8ea
hmphne <3rdoperson@gmail.com>
2022-09-23 15:08:28 +0330

    Update index.js

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,8 +1,7 @@
 const fs = require('fs')
 const http = require('http')
 const url = require('url')
-
-require('slugify')
+const slugify = require('slugify')
 // uses RELATIVE path
 const replaceTemplate = require('./modules/replaceTemplate')
 
@@ -43,6 +42,9 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }))
+console.log(slugs)
 
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse(req.url, true)
