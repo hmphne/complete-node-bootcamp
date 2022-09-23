3a5e06c9fce17b5e81e979e8f231a16658c22ebd
hmphne <3rdoperson@gmail.com>
2022-09-23 11:49:19 +0330

    session ??

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const http = require('http')
 const url = require('url')
 
+require('slugify')
 // uses RELATIVE path
 const replaceTemplate = require('./modules/replaceTemplate')
 
@@ -44,40 +45,40 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
-    const {query, pathname} = url.parse(req.url, true)
+    const { query, pathname } = url.parse(req.url, true)
 
-    
+
     // Overview  page
-    if(pathname === '/' || pathname === '/overview') {
-        res.writeHead(200, {'Content-type':'text/html'})
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, { 'Content-type': 'text/html' })
 
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
         const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
 
         res.end(output)
 
-    // Product page
-    } else if(pathname === '/product') {
-        res.writeHead(200, {'Content-type':'text/html'})
+        // Product page
+    } else if (pathname === '/product') {
+        res.writeHead(200, { 'Content-type': 'text/html' })
         const product = dataObj[query.id]
         const output = replaceTemplate(tempProduct, product)
         res.end(output)
 
-    // API
+        // API
     } else if (pathname === '/api') {
-        res.writeHead(200, {'Content-type':'application/json'})
+        res.writeHead(200, { 'Content-type': 'application/json' })
         res.end(data)
 
-    // NOT found
-    }else {
+        // NOT found
+    } else {
         res.writeHead(404, {
-            'Content-type':'text/html',
-            'my-own-header':'hello-world'
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
         })
         res.end('<h1>Page not found!</h1>')
     }
 })
 
-server.listen(8000, '127.0.0.1', ()=> {
+server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to requests on port 8000')
 })
