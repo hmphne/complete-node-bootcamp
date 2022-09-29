e62b3e6a0d1133fa821e8ad6f1f6744155ed8d48
hmphne <3rdoperson@gmail.com>
2022-09-29 14:39:20 +0330

    async/await

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -19,6 +19,23 @@ const writeFilePro = (file, data) => {
     })
 }
 
+const getDogPic = async () => {
+    try {
+        const data = await readFilePro(`${__dirname}/dog.txt`)
+        console.log(`Bread: ${data}`)
+    
+        const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+        console.log(res.body.message)
+    
+        await writeFilePro('dog-img.txt', res.body.message)
+        console.log('Random dog image saved to file!')
+    } catch (err) {
+        console.log(err)
+    }
+    
+}
+getDogPic()
+/*
 readFilePro(`${__dirname}/dog.txt`)
 .then((data) => {   
     console.log(`Bread: ${data}`) 
@@ -34,6 +51,4 @@ readFilePro(`${__dirname}/dog.txt`)
         console.log('Random dog image saved to file!')
 })
 .catch(err => console.log(err.message))
-
-
-  
+*/
