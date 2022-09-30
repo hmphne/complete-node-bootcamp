9c5b8a9a6dd9feb0a8b818ec1ffa9696df0f5467
hmphne <3rdoperson@gmail.com>
2022-09-30 14:29:02 +0330

    multiple mw

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const app = require('../app');
 const tourController = require('./../controllers/tourController');
 // const {getAllTours, createTour, getTour, updateTour, deleteTour} = require('./../controllers/tourController')
 
@@ -6,10 +7,15 @@ const router = express.Router()
 
 router.param('id', tourController.checkID)
 
+// Create a checkbody middleware
+// check if body contains the name and price properties
+// if not, send back 400 ( bad request)
+// add it to the post handler stack
+
 router
 .route('/')
 .get(tourController.getAllTours)
-.post(tourController.createTour)
+.post(tourController.checkBody, tourController.createTour)
 
 router
 .route('/:id')
