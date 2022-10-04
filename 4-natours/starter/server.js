0208025fd96715da3ee8a165d1a6f2b0413de023
hmphne <3rdoperson@gmail.com>
2022-10-04 12:23:31 +0330

    before creating a simple tour

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -7,14 +7,18 @@ dotenv.config({ path: './config.env'})
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 
-mongoose.connect(DB, {
+mongoose
+// .connect(DB, {
+
+.connect(process.env.DATABASE_LOCAL, {
     useNewUrlParser: true,
     useCreateIndex:true,
     useFindAndModify: false
-}).then(con => {
-    console.log(con.connections)
+}).then(() => {
     console.log('DB connection established')
 })
+
+
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
